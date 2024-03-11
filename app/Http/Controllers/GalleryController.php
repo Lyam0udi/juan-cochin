993f96e0ecd02a5a83fffb611612a778dadf4eca
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-03-11 00:03:34 +0000

    Controller Creation

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class GalleryController extends Controller
 {
-    //
+    public function index()
+    {
+        // Implement gallery logic
+    }
 }
