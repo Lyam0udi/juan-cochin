9a764ebad7227b32ca8171246e1964dd70c04e27
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-03-22 22:42:30 +0000

    Update SKills Display

--- resources/js/Components/Frontend/Skills.vue
+++ resources/js/Components/Frontend/Skills.vue
@@ -5,6 +5,8 @@ defineProps({
 </script>
 <template>
   <section class="bg-light-tail-100 dark:bg-dark-navy-500 py-16">
+    <div> Explorez mes Œuvres d'art</div>
+
     <div class="container mx-auto">
       <div class="grid grid-cols-8 md:grid-flow-col">
         <div
